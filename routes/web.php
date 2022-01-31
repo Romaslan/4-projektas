7c3914e465f1c572308eeff99688003630d5a171
Romaslan <93395762+Romaslan@users.noreply.github.com>
2022-01-31 19:24:31 +0200

    profileimages

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('profileimages')->group(function() {
+
+    // Route::get('', 'App\Http\Controllers\ProfileImageController@index')->name('profileimage.index');
+    Route::get('create', 'App\Http\Controllers\ProfileImageController@create')->name('profileimage.create');
+    // Route::post('store', 'App\Http\Controllers\ProfileImageController@store' )->name('profileimage.store');
+    // Route::get('edit/{profileImage}', 'App\Http\Controllers\ProfileImageController@edit')->name('profileimage.edit');
+    // Route::post('update/{profileImage}', 'App\Http\Controllers\ProfileImageController@update')->name('profileimage.update');
+    // Route::post('destroy/{company}', 'App\Http\Controllers\CompanyController@destroy' )->name('company.destroy');
+    // Route::get('show/{student}', 'App\Http\Controllers\StudentController@show')->name('student.show');
+
+});
